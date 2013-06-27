30ad61b77e11bdf53891562c0a0ed51d0c8bba04
shell@hpmini <shell909090@gmail.com>
2013-06-28 00:31:21 +0800

    - 移除错误提交的测试代码

--- schemepy/__main__.py
+++ schemepy/__main__.py
@@ -15,11 +15,8 @@ def print_src(filename):
 
 def compile_src(filename):
     with open(filename, 'r') as f: data = f.read()
-    for i in xrange(100):
-        code = schemepy.scompile(schemepy.split_code_tree(data.decode('utf-8')))
+    code = schemepy.scompile(schemepy.split_code_tree(data.decode('utf-8')))
     __import__('pprint').pprint(code)
-    # with open(path.splitext(filename)[0]+'.scc', 'wb') as fo:
-    #     cPickle.dump(code, fo, 2)
 
 def indent_src(filename, stream):
     with open(filename, 'r') as f: data = f.read()
